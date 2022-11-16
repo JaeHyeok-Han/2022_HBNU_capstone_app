8cbef382656406a617585a13d35977ba27b9783a
JaeHyeok-Han <hjh0633@gmail.com>
2022-11-16 11:21:18 +0900

    Feat: 스플래시 기능 추가

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 2000);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>안녕하세요.</Text>
